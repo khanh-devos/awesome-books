cfaa1fccba6d212109a2d44920b363b60d429d75
khanh-devos <dreamproperty.khanh@gmail.com>
2023-06-01 10:18:26 +0700

    fix linter errors

--- index.js
+++ index.js
@@ -40,14 +40,14 @@ class Book {
 
   static showPopWin() {
     const popWin = document.createElement('div');
-    popWin.className = 'pop-win'
+    popWin.className = 'pop-win';
     document.querySelector('body').appendChild(popWin);
-    
+
     popWin.innerHTML = `
     <div class="msg-win">
       <h4>New book successfully added</h4>
     </div>
-    `
+    `;
     setTimeout(() => popWin.remove(), 2000);
   }
 
